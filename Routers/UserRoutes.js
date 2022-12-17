132c0f0dafeff2e6ab113a20663cf0fad174bdff
xikam01 <cabdiraxmaanxikam190@gmail.com>
2022-12-17 08:58:42 -0800

    Added some Tbales

--- Routers/UserRoutes.js
+++ Routers/UserRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const  router = express.Router();
-const UserControl =require ('../controllers/Usercontrol')
+const UserControl = require ('../controllers/Usercontrol')
 
 router.post('/new', UserControl.Registertion)
+router.get('/get', UserControl.Getallusers)
+router.put('/:UserID', UserControl.UpdateUser)
 
 
 module.exports=router
--- controllers/Usercontrol.js
+++ controllers/Usercontrol.js
@@ -1,60 +1,250 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const bcrypt = require('bcrypt');
-const Jwt = require("jsonwebtoken")
+const Jwt = require('jsonwebtoken');
 
-//Token Generate
-
-const generatTokent = (Users) => {
-    return Jwt.sign({ Users }, process.env.Jwt_SEC, {
-        expiresIn: "10d"
+//Token Generator
+const generatetToken = (Users) => {
+    return Jwt.sign({ Users }, process.env.JWT_SEC, {
+        expiresIn: "40d"
     })
 }
 
+//==========================Registertion start============================>
 
-const Registertion = async (req, res, next) => {
+
+const Registertion = async (req, res) => {
     try {
-        const { Fname, Lname, Email, Password, Phone, Adderss } = req.body;
-        if (!Fname || !Lname || !Email || !Password || !Phone || !Adderss) {
+        const { fname, lname, email, password, phone, address } = req.body;
+
+
+        if (!fname || !lname || !email || !password || !phone || !address) {
             res.json({
-                status: "Something is wrong",
-                message: "Please Checking Data"
-            })
+                status: "Something is worng",
+                message: "please Checking Data"
+            });
             return;
+
         }
-        const Salt = bcrypt.genSaltSync(20)
-        const Hidepassword = bcrypt.hashSync(Password, Salt)
-        const NewUser = await prisma.Users.create({
+        const salt = bcrypt.genSaltSync(10)
+        const Hidepassword = bcrypt.hashSync(password, salt)
+        const Newuser = await prisma.Users.create({
             data: {
-                Firstname: Fname,
-                lastname: Lname,
-                u_email: Email,
+                Firstname: fname,
+                lastname: lname,
+                u_email: email,
                 u_password: Hidepassword,
-                u_phone: Phone,
-                u_addres: Adderss
+                u_phone: phone,
+                u_addres: address
             },
         });
-
-        req.json({
-            status: "Succes",
-            message: "SuccessFuly Registertion",
-            NewUser
-
-        })
-        const Token = generatTokent(NewUser.UserID)
         res.json({
-            user: { ...NewUser },
-            Token,
-            status: "Success"
+            status: "Success",
+            message: "Sucessfully save",
+            Newuser
         })
-
+        const token = generatetToken(Newuser.UserID)
+        res.json({
+            user: { ...Newuser },
+            token,
+            status: "Success",
+        });
     } catch (error) {
-        error
+        res.json({
+            error
+        })
+    }
+
+
+};
+
+
+
+
+
+//=========================================================Loging=========================================================================>
+
+const Login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.json({
+            status: "Error",
+            message: "email or password is wrong"
+        })
+        return;
+    }
+
+    const UserExisting = await prisma.Users.findFirst({
+        where: {
+            u_email: email
+        },
+    });
+
+    if (!UserExisting) {
+        res.json({
+            status: "Error",
+            message: " Wrong credentails"
+        });
+        return;
+    }
+
+    const dehshepassword = bcrypt.compareSync(password, UserExisting.u_password);
+    if (dehshepassword) {
+        const token = generatetToken(UserExisting.UserID);
+        res.json({
+            status: "SuccessFully",
+            message: "You  Are Login",
+            token,
+            user: UserExisting,
+        });
+
+
+    } else {
+       console.log(error)
     }
 }
 
 
 
+
+
+//========================================================update ==========================================================================>
+
+const UpdateUser = async (req, res, next) => {
+    try {
+        const { fname, lname, email, password, phone, address } = req.body;
+        const { UserID } = req.params
+        if (!fname || !lname || !email || !password || !phone || !address) {
+            res.json({
+                status: "Erorr",
+                message: "please checking Data "
+            })
+            return;
+        }
+        const FINDUser = await prisma.Users.findFirst({
+            where: {
+                UserID: + UserID,
+            }
+        });
+        if (!FINDUser) {
+            res.json({
+                status: "Erorr",
+                message: "User Is not Found In Database"
+            })
+            return
+        }
+        const updateusers = await prisma.Users.update({
+            where: {
+                UserID: parseInt(UserID)
+            },
+            data: {
+                Firstname: fname,
+                lastname: lname,
+                u_email: email,
+                u_password: password,
+                u_phone: phone,
+                u_addres: address
+            },
+        });
+        res.status(200).json({
+            status: "Sucess",
+            message: "Update Sucessfully",
+            updateusers
+        })
+    } catch (error) {
+        res.json({
+            status: "Erorr",
+        });
+    }
+};
+
+
+//=========================================================Get one user =================================================================>
+
+const GetOneuser = async (req, res) => {
+    try {
+        const { userID } = req.params;
+        const user = await prisma.users.findFirst({
+            where: {
+                userID: +userID,
+            },
+        });
+        if (!user) {
+            res.json({
+                status: "Erorr",
+                message: "user is not fount in Database Now"
+            });
+        } else {
+            res.json({
+                status: "Success",
+                user
+            })
+        }
+    } catch (error) {
+        res.json({
+            Error
+        });
+    };
+}
+
+
+
+
+
+
+
+
+//=========================================================Delete Users===================================================================>
+const DeleteUser = async (req, res,) => {
+    const { userID } = req.params;
+
+    const USRER = await prisma.users.delete({
+        where: {
+            userID: parseInt(userID)
+        },
+    });
+    res.json({
+        status: "Success",
+        message: "Users Delete SuccessFull!",
+        USRER
+    })
+}
+
+
+
+
+
+
+
+//=========================================================Geting Data====================================================================>
+const Getallusers = async (req, res) => {
+    try {
+        const USERS = await prisma.users.findMany();
+        res.json({
+            USERS
+        });
+    } catch (error) {
+        res.json({
+            status: "Error",
+            message: "Data is not Found"
+        });
+    }
+};
+
+
+
+
+
+
+
+//=========================================================Exports=======================================================================>
+
 module.exports = {
-    Registertion
+    Registertion,
+    Login,
+    Getallusers,
+    GetOneuser,
+    DeleteUser,
+    UpdateUser
 }
