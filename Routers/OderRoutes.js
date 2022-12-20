8f6c44a372b7ad89ef1bc862da7cb2e53f122938
xikam01 <cabdiraxmaanxikam190@gmail.com>
2022-12-20 00:12:48 -0800

    Added Orders

--- Routers/OderRoutes.js
+++ Routers/OderRoutes.js
@@ -4,5 +4,6 @@ const OdersControls =require('../controllers/Orders');
 const { protect } = require('../middleware/Auth');
 
 router.post('/Createoders',protect, OdersControls.CreateOders)
+router.get('/getall' , OdersControls.Getallorders)
 
 module.exports= router
--- controllers/Orders.js
+++ controllers/Orders.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
-
+//====================================================================>>CreateOrders
 const CreateOders = async (req, res, next) => {
     try {
         const { D_price, Item_price, Total_price, Address } = req.body;
@@ -37,8 +37,74 @@ const CreateOders = async (req, res, next) => {
     }
 }
 
+//====================================================================>>UpdateOders
+const UpdateOrders = async (req, res, next) => {
+    try {
+        const { D_price, Item_price, Total_price, Address } = req.body;
+        const { Ored_id } = req.params
+        if (!D_price ||!Item_price ||!Total_price ||!Address) {
+            res.json({
+                status: "Erorr",
+                message: "please checking Data "
+            })
+            return;
+        }
+        const Findorders = await prisma.Oreds.findFirst({
+            where: {
+                Ored_id: + Ored_id,
+            }
+        });
+        if (!Findorders) {
+            res.json({
+                status: "Erorr",
+                message: "Oders Is not Found In Database"
+            })
+            return
+        }
+        const updateOrders = await prisma.Oreds.update({
+            where: {
+                Ored_id: parseInt(Ored_id)
+            },
+            data: {
+                Delivery_price: D_price,
+                Item_price: Item_price,
+                total_price: Total_price,
+                address: Address,
+            },
+        });
+        res.status(200).json({
+            status: "Sucess",
+            message: "Update Sucessfully",
+            updateOrders
+        })
+    } catch (error) {
+        res.json({
+            status: "Erorr",
+            message:"DOne"
+        });
+    }
+};
+
+//=====================================================================>>GetAllorders
+
+
+const Getallorders = async (req, res) => {
+    try {
+        const getorders = await prisma.Oreds.findMany();
+        res.json({
+            getorders
+        });
+    } catch (error) {
+        res.json({
+            status: "Error",
+            message: "Data is not Found"
+        });
+    }
+};
 
 
 module.exports={
-    CreateOders
+    CreateOders,
+    UpdateOrders,
+    Getallorders
 }
